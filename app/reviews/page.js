d853b4bace9a04c7b9fa68b5b417915ee97a4b3d
DANIEL SUAREZ <danielsuarez@MacBook-Pro-2.lan>
2023-07-13 22:02:06 -0400

    fix

--- app/reviews/page.js
+++ app/reviews/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { reviewsWidget } from "../widgets";
 
-export default function () {
+export default function page() {
   return (
     <div className="max-w-screen-lg mx-auto py-32 min-h-screen">
       <div dangerouslySetInnerHTML={{ __html: reviewsWidget }}></div>
