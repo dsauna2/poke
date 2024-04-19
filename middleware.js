08abc4e8d4ad0721cfcbdae0ee5427e454925533
DANIEL SUAREZ <danielsuarez@MacBook-Pro-2.lan>
2024-04-19 18:55:55 -0400

    limit middleware

--- middleware.js
+++ middleware.js
@@ -35,14 +35,5 @@ export async function middleware(request) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/", "/overview", "/location"],
 };
